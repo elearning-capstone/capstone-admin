e0ca34127431f4b144818d9ba9c8a775aeb1c135
noof3310 <noof3310@gmail.com>
2022-02-17 14:07:58 +0700

    fix route, axios req

--- routes/user.js
+++ routes/user.js
@@ -4,18 +4,18 @@ const router = express.Router();
 
 const user_ip = "http://ip-172-31-33-253.ap-southeast-1.compute.internal:3000";
 
-router.get("/user", async (req, res) => {
+router.get("/", async (req, res) => {
     try {
-        const response = await axios.post(user_ip + "/user", req.body);
+        const response = await axios.get(user_ip + "/user");
         return res.json(response.data);
     } catch (err) {
         return res.status(err.response.status || 404).json(err.response.data || { message: "not found" });
     }
 });
 
-router.put("/user/role", async (req, res) => {
+router.put("/role", async (req, res) => {
     try {
-        const response = await axios.post(user_ip + "/user/role", req.body);
+        const response = await axios.put(user_ip + "/user/role", req.body);
         return res.json(response.data);
     } catch (err) {
         return res.status(err.response.status || 404).json(err.response.data || { message: "not found" });
