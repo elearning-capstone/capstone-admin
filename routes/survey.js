ed054685964fac6b9c8401a2cf176e346f1e695b
kttkkkng <teerawat7971@gmail.com>
2022-03-15 21:19:54 +0700

    survey create

---
+++ routes/survey.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const axios = require("axios");
+const router = express.Router();
+
+const survey_ip = "ip-172-31-37-162.ap-southeast-1.compute.internal:3000";
+
+router.post("/create", async (req, res) => {
+    try {
+        const response = await axios.post(survey_ip + "/survey/create", req.body);
+        return res.json(response.data);
+    } catch (err) {
+        return res.status(err.response.status || 404).json(err.response.data || { message: "not found" });
+    }
+});
+
+module.exports = router;
